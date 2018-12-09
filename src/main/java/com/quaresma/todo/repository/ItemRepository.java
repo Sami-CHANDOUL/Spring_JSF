9cb44788cb04290e515569e9b7576063bf95fe0e
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2018-12-08 23:14:21 -0300

    Fix

--- src/main/java/com/quaresma/todo/repository/ItemRepository.java
+++ src/main/java/com/quaresma/todo/repository/ItemRepository.java
@@ -9,10 +9,12 @@ import com.quaresma.todo.model.Item;
 
 public interface ItemRepository extends JpaRepository<Item, Integer> {
 
-	@Query(value = "SELECT * FROM todos_esig.item i WHERE i.status = false", nativeQuery = true)
+	//@Query(value = "SELECT * FROM todos_esig.item i WHERE i.status = false", nativeQuery = true)
+	@Query(value = "SELECT * FROM item i WHERE i.status = false", nativeQuery = true)
 	List<Item> findItemsActives();
 
-	@Query(value = "SELECT * FROM todos_esig.item i WHERE i.status = true", nativeQuery = true)
+	//@Query(value = "SELECT * FROM todos_esig.item i WHERE i.status = true", nativeQuery = true)
+	@Query(value = "SELECT * FROM item i WHERE i.status = true", nativeQuery = true)
 	List<Item> findItemsCompleted();
 
 }
