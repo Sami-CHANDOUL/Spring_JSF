c1a78b157f2f98a799e678dd4b19320be96c55f0
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2018-12-09 15:45:28 -0300

    Rest service, POST

--- src/main/java/com/quaresma/todo/restService/ItemControllerRest.java
+++ src/main/java/com/quaresma/todo/restService/ItemControllerRest.java
@@ -3,19 +3,19 @@ package com.quaresma.todo.restService;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.quaresma.todo.dao.ItemDao;
 import com.quaresma.todo.model.Item;
-import com.quaresma.todo.restService.response.Response;
 
 @RestController
 @RequestMapping("/item")
@@ -24,18 +24,24 @@ public class ItemControllerRest {
 
 	@Autowired
 	private ItemDao itemDao;
-	
+
 	@GetMapping
 	@ResponseBody
 	public List<Item> findAll() {
 
 		List<Item> items = new ArrayList<Item>();
-		
-		items = itemDao.findAll();		
-		
+
+		items = itemDao.findAll();
+
 		return items;
 
 	}
-	
-	
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	@ResponseBody
+	public void create(@RequestBody Item item) {
+		itemDao.saveOrUpdate(item);
+	}
+
 }
