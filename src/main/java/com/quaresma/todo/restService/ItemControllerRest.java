49d6f2d5cf886897a90be19e89e2329944fe90ba
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2018-12-09 23:32:03 -0300

    Fix POST Method

--- src/main/java/com/quaresma/todo/restService/ItemControllerRest.java
+++ src/main/java/com/quaresma/todo/restService/ItemControllerRest.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +18,7 @@ import com.quaresma.todo.dao.ItemDao;
 import com.quaresma.todo.model.Item;
 
 @RestController
-@RequestMapping(value = "/item", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+@RequestMapping("/item")
 @CrossOrigin(origins = "*")
 public class ItemControllerRest {
 
