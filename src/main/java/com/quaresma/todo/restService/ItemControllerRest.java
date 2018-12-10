a5ff069a524e4a14cb6492ec2df325a01d607f10
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2018-12-10 00:08:44 -0300

    Fix POST Method

--- src/main/java/com/quaresma/todo/restService/ItemControllerRest.java
+++ src/main/java/com/quaresma/todo/restService/ItemControllerRest.java
@@ -5,11 +5,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +34,8 @@ public class ItemControllerRest {
 		return items;
 	}
 
-	@PostMapping
+	@RequestMapping(method = RequestMethod.POST,
+	        produces = {MediaType.APPLICATION_JSON_VALUE})
 	@ResponseStatus(HttpStatus.CREATED)
 	@ResponseBody
 	public void create(@RequestBody Item item) {
